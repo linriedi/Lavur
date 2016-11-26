8a308fc0f747b77346d99819093c7719cda5b74e
Riedi Linus <linriedi@hotmail.com>
2016-11-26 15:48:44 +0100

    Create solution LavuTrials

---
+++ LavurTrials/BackEnd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
